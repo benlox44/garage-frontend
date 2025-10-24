3d083e34e70aadc48283a78ede8adb0ac569ea08
Javier Saldaño <javier.saldano@alumnos.ucn.cl>
2025-10-24 17:23:18 -0300

    chore(global): Configuracion de vue + vite + taildwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="text-red-500 font-bold text-xl">¡Si ves esto rojo, Tailwind funciona!</div>
   </div>
 </template>
 
